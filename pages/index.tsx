31507c4be578bf229ac71fc0d4ca729028a12fe2
Lester <lgdelacruz92@gmail.com>
2024-08-07 10:57:39 -0400

    get page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [collegeNames, setCollegeNames] = useState<string[]>([]);
   const getPage = async () => {
-    const res = await fetch("http://localhost:3000/api/getPage");
+    const res = await fetch("/api/getPage");
     const result = await res.json();
     setCollegeNames(result.collegeNames);
   };
