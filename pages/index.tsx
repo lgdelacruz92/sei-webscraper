5fb8a4517550f76799b1071b739ea8995a36c47b
Lester <lgdelacruz92@gmail.com>
2024-08-08 09:20:27 -0400

    adding loading bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingBar from "@/components/loading-bar";
 import { Button, TextInput } from "@mantine/core";
 
 const url = "https://bigfuture.collegeboard.org/college-search";
@@ -8,7 +9,7 @@ export default function Home() {
   const onStartScraping = () => {};
   return (
     <main>
-      <div className="app-container flex flex-col items-center">
+      <div className="app-container flex flex-col items-center gap-y-4">
         <TextInput
           label="URL"
           placeholder={url}
@@ -16,6 +17,7 @@ export default function Home() {
           className="m-4 min-w-[80%]"
         />
         <Button onClick={onStartScraping}>Start Scraping</Button>
+        <LoadingBar progress={50} description="Scraping College List" />
       </div>
     </main>
   );
